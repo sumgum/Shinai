02d96b5ef860861ecc4f7e473a5ec70b18cbfc07
Sumi Gunyu <sumkakkyo@gmail.com>
2020-02-05 07:33:21 +0900

    初期修正

--- public/js/app.js
+++ public/js/app.js
@@ -45158,8 +45158,8 @@ $(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/Shinai/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/Shinai/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/shinai_pro/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/shinai_pro/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
